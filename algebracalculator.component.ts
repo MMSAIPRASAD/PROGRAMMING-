5637e627bb28beb859aff5947049c0891d6a7631
MMSAIPRASAD <70465595+MMSAIPRASAD@users.noreply.github.com>
2020-09-08 17:11:06 +0530

    Add files via upload

---
+++ algebracalculator.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-algebracalculator',
+  templateUrl: './algebracalculator.component.html',
+  styleUrls: ['./algebracalculator.component.css']
+})
+export class AlgebracalculatorComponent implements OnInit {
+
+  
+    
+  
+  constructor() { }
+
+  ngOnInit(): void {
+
+
+  }
+
+}
